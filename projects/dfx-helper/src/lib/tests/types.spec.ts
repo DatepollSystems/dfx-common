86b9f01a04ec4b0c68365a5092e85a969646f2a4
Dominik <git@dafnik.me>
2022-01-20 14:16:12 +0100

    fix(helper): parseKeyPairsToHttpParams and add unit tests

--- projects/dfx-helper/src/lib/tests/types.spec.ts
+++ projects/dfx-helper/src/lib/tests/types.spec.ts
@@ -152,3 +152,96 @@ describe('Types KeyValuePair.parseIntoUrl', () => {
     }).not.toThrow('Url does not match all KeyValuePairs');
   });
 });
+
+describe('Types KeyValuePair.parseIntoParams', () => {
+  it('parse multiple numbers', () => {
+    const params = KeyValuePair.parseIntoHttpParams([
+      {key: 'uId', value: 1},
+      {key: 'miaId', value: 2},
+    ]);
+    console.log(params);
+    expect(params).toBeDefined();
+    expect(params?.get('uId')).toBe('1');
+    expect(params?.get('miaId')).toBe('2');
+  });
+
+  it('parse multiple strings', () => {
+    const params = KeyValuePair.parseIntoHttpParams([
+      {key: 'uId', value: 'wow'},
+      {
+        key: 'miaId',
+        value: 'lol',
+      },
+    ]);
+    expect(params).toBeDefined();
+    expect(params?.get('uId')).toBe('wow');
+    expect(params?.get('miaId')).toBe('lol');
+  });
+
+  it('parse multiple booleans', () => {
+    const params = KeyValuePair.parseIntoHttpParams([
+      {key: 'uId', value: false},
+      {
+        key: 'miaId',
+        value: true,
+      },
+    ]);
+    expect(params).toBeDefined();
+    expect(params?.get('uId')).toBe('false');
+    expect(params?.get('miaId')).toBe('true');
+  });
+  it('parse multiple types', () => {
+    const params = KeyValuePair.parseIntoHttpParams([
+      {key: 'uId', value: false},
+      {key: 'miaId', value: 'wow'},
+      {
+        key: 'try',
+        value: 1,
+      },
+    ]);
+    expect(params).toBeDefined();
+    expect(params?.get('uId')).toBe('false');
+    expect(params?.get('miaId')).toBe('wow');
+    expect(params?.get('try')).toBe('1');
+  });
+  it('parse single', () => {
+    const params = KeyValuePair.parseIntoHttpParams([{key: 'uId', value: 'ashdkgfkagewrqkhwgqjvad'}]);
+    expect(params).toBeDefined();
+    expect(params?.get('uId')).toBe('ashdkgfkagewrqkhwgqjvad');
+  });
+  it('contains duplicates fail', () => {
+    expect(function () {
+      KeyValuePair.parseIntoHttpParams([
+        {key: 'uId', value: 'wow'},
+        {
+          key: 'uId',
+          value: 'lol',
+        },
+      ]);
+    }).toThrow('KeyValuePairs contains duplicates');
+  });
+  it('parse types with null', () => {
+    const params = KeyValuePair.parseIntoHttpParams([
+      {key: 'uId', value: null},
+      {key: 'miaId', value: 'wow'},
+      {
+        key: 'try',
+        value: 1,
+      },
+      {key: 'bId', value: undefined},
+    ]);
+    expect(params).toBeDefined();
+    expect(params?.get('uId')).toBeNull();
+    expect(params?.get('bId')).toBeNull();
+    expect(params?.get('miaId')).toBe('wow');
+    expect(params?.get('try')).toBe('1');
+  });
+  it('parse undefined or null key value pair', () => {
+    expect(KeyValuePair.parseIntoHttpParams(null)).toBeUndefined();
+    expect(KeyValuePair.parseIntoHttpParams(undefined)).toBeUndefined();
+    expect(KeyValuePair.parseIntoHttpParams([])).toBeUndefined();
+  });
+  it('parse empty array', () => {
+    expect(KeyValuePair.parseIntoHttpParams([])).toBeUndefined();
+  });
+});
