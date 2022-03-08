213bee0a8afc5823a42be0effffb103aaa366e56
Dominik <git@dafnik.me>
2022-03-08 16:33:10 +0100

    feat(helper): return string on clipboard read, add new method for copying

--- projects/dfx-helper/src/lib/helper/ClipboardHelper.ts
+++ projects/dfx-helper/src/lib/helper/ClipboardHelper.ts
@@ -1,22 +1,24 @@
 export class ClipboardHelper {
   public static copy(value: string): void {
-    const selBox = document.createElement('textarea');
-    selBox.style.position = 'fixed';
-    selBox.style.left = '0';
-    selBox.style.top = '0';
-    selBox.style.opacity = '0';
-    selBox.value = value;
-    document.body.appendChild(selBox);
-    selBox.focus();
-    selBox.select();
-    document.execCommand('copy');
-    document.body.removeChild(selBox);
+    try {
+      void navigator.clipboard.writeText(value);
+    } catch {
+      const copyBox = document.createElement('textarea');
+      copyBox.style.position = 'fixed';
+      copyBox.style.left = '0';
+      copyBox.style.top = '0';
+      copyBox.style.opacity = '0';
+      copyBox.value = value;
+      document.body.appendChild(copyBox);
+      copyBox.focus();
+      copyBox.select();
+      copyBox.setSelectionRange(0, 99999);
+      document.execCommand('copy');
+      document.body.removeChild(copyBox);
+    }
   }
 
-  /**
-   * @deprecated
-   */
-  public static read(): Promise<any> {
+  public static read(): Promise<string> {
     return navigator.clipboard.readText();
   }
 }
