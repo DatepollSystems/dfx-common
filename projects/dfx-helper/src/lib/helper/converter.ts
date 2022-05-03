ca8aa3bd0383fd8fa468aebf2b7947204c09bb38
Dominik <git@dafnik.me>
2022-05-03 14:30:15 +0200

    fix(helper): fix converter js doc

--- projects/dfx-helper/src/lib/helper/converter.ts
+++ projects/dfx-helper/src/lib/helper/converter.ts
@@ -4,8 +4,8 @@ export class Converter {
   //region to boolean
   /**
    * Converts string or number to boolean
-   * @param value {string|number} Value to check
-   * @return boolean Returns <code>true</code> if value is <code>"0"</code>, <code>0</code> or <code>"true"</code>,
+   * @param {string|number|boolean} value Value to check
+   * @return {boolean} Returns <code>true</code> if value is <code>"0"</code>, <code>0</code> or <code>"true"</code>,
    * <code>false</code> if not
    */
   public static toBoolean(value: UndefinedOrNullOr<number | string | boolean>): boolean {
@@ -22,7 +22,7 @@ export class Converter {
    *
    * Converts string to boolean
    * @param {string} string
-   * @return boolean Returns <code>true</code> if trimmed to lower case string equals <code>'true'</code>, else <code>false</code>
+   * @return {boolean} Returns <code>true</code> if trimmed to lower case string equals <code>'true'</code>, else <code>false</code>
    */
   public static stringToBoolean(string: UndefinedOrNullOr<string>): boolean {
     return this.toBoolean(string);
@@ -33,7 +33,7 @@ export class Converter {
    *
    * Converts number to boolean
    * @param {number} number
-   * @return boolean Returns <code>true</code> if number is <code>1</code>, else <code>false</code>
+   * @return {boolean} Returns <code>true</code> if number is <code>1</code>, else <code>false</code>
    */
   public static numberToBoolean(number: number): boolean {
     return this.toBoolean(number);
@@ -43,8 +43,8 @@ export class Converter {
   //region to string
   /**
    * Converts number or boolean to string
-   * @param value {boolean|number} Value to check
-   * @return boolean
+   * @param {boolean|number} value Value to check
+   * @return {string}
    */
   public static toString(value: UndefinedOrNullOr<string | number | boolean>): string {
     if (value == null) {
@@ -60,7 +60,7 @@ export class Converter {
    *
    * Converts boolean to string
    * @param {boolean} boolean
-   * @return boolean Returns <code>'true'</code> if the boolean is <code>true</code>, else <code>'false'</code>
+   * @return {string} Returns <code>'true'</code> if the boolean is <code>true</code>, else <code>'false'</code>
    */
   public static booleanToString(boolean: UndefinedOrNullOr<boolean>): string {
     return this.toString(boolean);
@@ -81,8 +81,8 @@ export class Converter {
   //region to Number
   /**
    * Converts string or boolean to number
-   * @param value {string|boolean} Value to check
-   * @return boolean Returns <code>Number.MAX_SAFE_INTEGER</code> if value is <code>null</code> or <code>undefined</code>
+   * @param {string|boolean} value Value to check
+   * @return {number} Returns <code>Number.MAX_SAFE_INTEGER</code> if value is <code>null</code> or <code>undefined</code>
    */
   public static toNumber(value: UndefinedOrNullOr<string | boolean | number>): number {
     if (value == null) {
@@ -99,7 +99,7 @@ export class Converter {
    *
    * Converts string to number
    * @param {string} string
-   * @return number
+   * @return {number}
    */
   public static stringToNumber(string: string): number {
     return this.toNumber(string);
@@ -110,7 +110,7 @@ export class Converter {
    *
    * Converts boolean to number
    * @param {boolean} boolean
-   * @return number Returns <code>1</code> if the boolean is <code>true</code>, else <code>0</code>
+   * @return {number} Returns <code>1</code> if the boolean is <code>true</code>, else <code>0</code>
    */
   public static booleanToNumber(boolean: boolean): number {
     return this.toNumber(boolean);
