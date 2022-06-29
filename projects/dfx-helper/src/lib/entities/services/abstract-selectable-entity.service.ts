5de06a6a7c6a6276316c3c587a1eddd48f1b3a99
Dominik <git@dafnik.me>
2022-06-29 20:19:31 +0200

    refactor(helper): ref refresh selected entity on fetchAll

--- projects/dfx-helper/src/lib/entities/services/abstract-selectable-entity.service.ts
+++ projects/dfx-helper/src/lib/entities/services/abstract-selectable-entity.service.ts
@@ -21,6 +21,14 @@ export abstract class ASelectableEntityService<idType extends StringOrNumber, En
 
   protected constructor(httpService: AHttpService, url: string) {
     super(httpService, url);
+
+    this.allChange.subscribe((entities) => {
+      entities.forEach((entity) => {
+        if (this.selected?.id === entity.id) {
+          this.setSelected(entity);
+        }
+      });
+    });
   }
 
   public getSelected(): UndefinedOr<EntityType> {
@@ -38,24 +46,5 @@ export abstract class ASelectableEntityService<idType extends StringOrNumber, En
     this.selected = selected;
     StorageHelper.set(this.selectedStorageKey, this.selected);
     this.selectedChange.next(this.selected);
-  }
-
-  public override fetchAll(urlKeyPairs?: KeyValuePair[], params?: KeyValuePair[]): void {
-    const url = KeyValuePair.parse(this.globalGetAllUrl, urlKeyPairs);
-    const httpParams = KeyValuePair.parseIntoHttpParams(params ? params : this.globalGetAllParams);
-    this.httpService.get(url ? url : this.globalGetAllUrl ? this.globalGetAllUrl : this.url, httpParams, 'fetchAll').subscribe({
-      next: (data: any) => {
-        const entities = new EntityList<EntityType>();
-        for (const dto of data) {
-          const entity = this.convert(dto);
-          if (this.selected?.id === entity.id) {
-            this.setSelected(entity);
-          }
-          entities.push(entity);
-        }
-        this.setAll(entities);
-      },
-      error: (error) => console.log(error),
-    });
   }
 }
