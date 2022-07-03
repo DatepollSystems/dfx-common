a87ad6adb1605cfa2d7c887ba9d79b7d179d01c4
Dominik <git@dafnik.me>
2022-07-03 16:55:45 +0200

    refactor(helper): refactor remember results

--- projects/dfx-helper/src/lib/decorators/remember-result.ts
+++ projects/dfx-helper/src/lib/decorators/remember-result.ts
@@ -5,13 +5,18 @@
  */
 export function RememberResult(target: Object, propertyKey: string, descriptor: PropertyDescriptor) {
   const ogMethod = descriptor.value;
-  descriptor.value = function(...args: any[]) {
+  descriptor.value = function (...args: any[]) {
     let uniqueArgsIdentifier = '';
     for (const arg of args) {
       uniqueArgsIdentifier += arg.toString();
     }
-    ogMethod.cachedResult = ogMethod.cachedResult || {};
-    return ogMethod.cachedResult[uniqueArgsIdentifier] ? ogMethod.cachedResult[uniqueArgsIdentifier] : (ogMethod.cachedResult[uniqueArgsIdentifier] = ogMethod.apply(this, args));
+    if (!ogMethod.cachedResult) {
+      ogMethod.cachedResult = {};
+    }
+    if (!ogMethod.cachedResult[uniqueArgsIdentifier]) {
+      ogMethod.cachedResult[uniqueArgsIdentifier] = ogMethod.apply(this, args);
+    }
+    return ogMethod.cachedResult[uniqueArgsIdentifier];
   };
   return descriptor;
 }
