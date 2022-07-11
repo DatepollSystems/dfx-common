294e37f29a8e1abb00012ab9065c4b43bc2d5f80
Dominik <git@dafnik.me>
2022-07-11 19:42:30 +0200

    feat(helper): add ignore paths to base url interceptor

--- projects/dfx-helper/src/lib/helper.config.ts
+++ projects/dfx-helper/src/lib/helper.config.ts
@@ -3,6 +3,7 @@ import {InjectionToken} from '@angular/core';
 export interface HelperConfig {
   isMobileBreakpoint?: number;
   baseUrl?: string;
+  baseUrlIgnorePaths?: string[];
 }
 
 export const HELPER_CONFIG = new InjectionToken('HELPER_CONFIG');
--- projects/dfx-helper/src/lib/interceptor/base-url-interceptor.ts
+++ projects/dfx-helper/src/lib/interceptor/base-url-interceptor.ts
@@ -9,13 +9,25 @@ import {LoggerFactory} from '../helper/logger';
 export class BaseUrlInterceptor implements HttpInterceptor {
   private lumber = LoggerFactory.getLogger('httpClient');
   baseUrl?: string;
+  baseUrlIgnorePaths?: string[];
 
   constructor(@Inject(HELPER_CONFIG) private config: HelperConfig) {
     this.baseUrl = config.baseUrl;
+    this.baseUrlIgnorePaths = config.baseUrlIgnorePaths;
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (this.baseUrl && this.baseUrl.length > 0) {
+    let toIntercept = true;
+    if (this.baseUrlIgnorePaths) {
+      for (const path of this.baseUrlIgnorePaths) {
+        if (req.url.includes(path)) {
+          toIntercept = false;
+          break;
+        }
+      }
+    }
+
+    if (toIntercept && this.baseUrl && this.baseUrl.length > 0) {
       return next.handle(req.clone({url: `${this.baseUrl}/${req.url}`}));
     } else {
       this.lumber.warning('intercept', 'It looks like you are intercepting requests to add a base url but you did not define one.');
