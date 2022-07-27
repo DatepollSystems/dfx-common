56389118b607e4c4367c49de4ca11ac37b73127a
Dominik <git@dafnik.me>
2022-07-27 13:39:49 +0200

    feat(helper): make trackById more generic, add trackByIndex

--- projects/dfx-helper/src/lib/directives/track-by.directive.ts
+++ projects/dfx-helper/src/lib/directives/track-by.directive.ts
@@ -1,35 +1,42 @@
 import {Directive, Host, Input, NgIterable, NgModule} from '@angular/core';
 import {NgForOf} from '@angular/common';
-
 import {StringOrNumber} from '../types';
-import {IEntity} from '../entities/entity.interface';
 
 @Directive({
   selector: '[ngForTrackByProperty]',
 })
 export class TrackByPropertyDirective<T, U extends NgIterable<T> = NgIterable<T>> {
-  @Input('ngForTrackByProperty') propertyName: keyof T | undefined;
+  @Input('ngForTrackByProperty') propertyName!: keyof T;
 
   // We don't use this, we just need it so Ivy will give us a type for T
-  @Input() ngForOf: (U & NgIterable<T>) | undefined | null;
+  @Input() ngForOf!: U & NgIterable<T>;
 
-  public constructor(@Host() private readonly ngFor: NgForOf<T, U>) {
-    this.ngFor.ngForTrackBy = (index: number, item: T) => (this.propertyName ? item[this.propertyName] : item);
+  public constructor(@Host() ngFor: NgForOf<T, U>) {
+    ngFor.ngForTrackBy = (index: number, item: T) => item[this.propertyName];
   }
 }
 
 @Directive({
   selector: '[ngForTrackById]',
 })
-export class TrackByIdDirective<T extends IEntity<StringOrNumber>> {
-  constructor(@Host() private readonly ngFor: NgForOf<T>) {
-    this.ngFor.ngForTrackBy = (index: number, item: T) => item.id;
+export class TrackByIdDirective<T extends {id: StringOrNumber}> {
+  constructor(@Host() ngFor: NgForOf<T>) {
+    ngFor.ngForTrackBy = (_: number, item: T) => item.id;
+  }
+}
+
+@Directive({
+  selector: '[ngForTrackByIndex]',
+})
+export class TrackByIndexDirective {
+  constructor(@Host() ngFor: NgForOf<any>) {
+    ngFor.ngForTrackBy = (index: number) => index;
   }
 }
 
 @NgModule({
   imports: [],
-  declarations: [TrackByIdDirective, TrackByPropertyDirective],
-  exports: [TrackByIdDirective, TrackByPropertyDirective],
+  declarations: [TrackByIdDirective, TrackByPropertyDirective, TrackByIndexDirective],
+  exports: [TrackByIdDirective, TrackByPropertyDirective, TrackByIndexDirective],
 })
 export class DfxTrackByModule {}
